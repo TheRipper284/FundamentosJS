f4858bd7a962dbcfe1470003cd94371b80d92d0b
TherRipper482 <santiagogustavo882@gmail.com>
2023-09-19 11:54:57 -0600

    09 Ciclos en JS

---
+++ ciclos.js
@@ -0,0 +1,32 @@
+// Ciclos en JavaScript
+
+// for
+for(let i = 0; i < 3; i++){
+    console.log('vuelta' + 1);
+}
+
+// while
+let contador = 0;
+while(contador < 3){
+    console.log('vuelta' + contador);
+    contador++;
+}
+
+// do wile
+contador = 0;
+do{
+    console.log('vuelta' + contador);
+    contador++;
+} while(contador === 0);
+
+// recorrido de un string
+const name = 'Nexxuz';
+for(let i = 0; i <name.legth; i++){
+    console.log(name[i]);
+}
+
+// recorrido de un array
+const numbers = [1,2,3,4,5,6];
+for(let i = 0; i < numbers.length;i++){
+    console.log(numbers[i]);
+}
