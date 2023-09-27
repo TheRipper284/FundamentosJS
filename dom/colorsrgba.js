dded7cc950996d5f0e3180245a64decb733a54a6
TherRipper482 <santiagogustavo882@gmail.com>
2023-09-27 10:49:42 -0600

    16 DOM Galeria Bootstrap

--- dom/colorsrgba.js
+++ dom/colorsrgba.js
@@ -1,5 +1,5 @@
 const body = document.querySelector('body');
-// const btnColor = document.querySelector('#btnColor');
+const btnColor = document.querySelector('#btnColor');
 const red = document.querySelector('#red');
 const green = document.querySelector('#green');
 const blue = document.querySelector('#blue');
